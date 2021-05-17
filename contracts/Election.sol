449e60e8aae712e617a41ecd93f5d7510a350ea5
darshanraju <darshan.raju99@gmail.com>
2021-05-17 15:56:03 +1000

    Added comments

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -9,31 +9,25 @@ contract Election {
         uint voteCount;
     }
 
-    //Store candidate
-    //Fetch Candidate
+    //Create map from candidateID to candidateObject
     mapping(uint => Candidate) public candidates;
 
-    //Store voters
+    //Create map from votiers address to bool, which signifies is they voted or not yet
     mapping(address => bool) public voters;
 
-    //Store Candidates Count
+    //NOTE: created a getter for it (e.g. app.candidate() gives 'Candidate 1')
     uint public candidatesCount;
+    // string public candidate;
 
-    //Read candidate, setting the state variable public automatically 
-    //created a getter for it (e.g. app.candidate() gives 'Candidate 1')
-
-    string public candidate;
 
     //Constructor
     constructor () {
-        //State variable
         //Add the addCandidate function inside the constructor, so it runs whenever the contracted is
         //migrated or deployed
 
         //This smart contract will be in charge of knowing how many candidates exist in the Election 
         addCandidate("Candidate 1");
         addCandidate("Candidate 2");
-
     }
 
     //the underscore (_) means this is a local variable, not a state variable
@@ -43,10 +37,9 @@ contract Election {
     }   
 
     function vote (uint _candidateId) public {
-        //Check for valid scenario
         require(!voters[msg.sender]);
         require(_candidateId > 0 && _candidateId < candidatesCount);
-        //msg is meta data about who sent the vote
+        //NOTE: is meta data about who sent the vote
 
         //NOTE: ANY CODE BELOW THIS THAT ISN"T REACHED DUE TO FAILURES OF THE REQUIRES, THE GAS WILL BE REFUNDED
         //      ANY CODE ABOVE THAT HITS THE REQUIRES AND FAILS WILL NOT BE REFUNDED, AS THAT CODE WAS EXECUTED
